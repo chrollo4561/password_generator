81937b15d6e43d9a8f363fd480fd6ed7532155c4
chrollo4561 <singhayush4561@gmail.com>
2025-06-22 11:55:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
-const slider = document.getElementById("slider");
-const lengthDisplay = document.getElementById("lengthDisplay");
-const resultBox = document.getElementById("result");
+function generatePassword(){
+    var characterset= "";
+    if(document.getElementById("addLetters").checked){
+        characterset += "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+    }
+    if(document.getElementById("addNumbers").checked){
+        characterset += "1234567890";
+    }
+    if(document.getElementById("addSymbols").checked){
+        characterset += "!@#$%^&*()+_-=;:?,.";
+    }
+    
+    var password = "";
 
-lengthDisplay.textContent = slider.value;
+    if(characterset === ""){
+        alert("Select atleast one option");
+        return;
+    }
 
-slider.addEventListener("input", () => {
-lengthDisplay.textContent = slider.value;
-});
+    var length = parseInt(document.getElementById("length").value);
+    for(var i=0; i<length ; i++){
+        password += characterset[Math.floor(Math.random()*characterset.length)];
+    }
 
-function generatePassword() {
-let characters = "";
-const letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
-
-if (document.getElementById("includeLetters").checked) {
-    characters += letters;
-}
-if (document.getElementById("includeNumbers").checked) {
-    characters += numbers;
-}
-if (document.getElementById("includeSymbols").checked) {
-    characters += symbols;
-}
-
-if (characters === "") {
-    alert("Please select at least one option!");
-    return;
+    document.getElementById("output").value= password;
 }
 
-const length = parseInt(slider.value);
-let password = "";
-for (let i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
-}
-
-resultBox.value = password;
-}
-
-function copyToClipboard() {
-if (resultBox.value === "") {
-alert("Please select at least one option!");
-return;
-}
-resultBox.select();
-document.execCommand("copy");
-alert("Password copied to clipboard!");
+function copyToClipboard(){
+    if(document.getElementById("output").value === ""){
+        alert("nothing in there to copy");
+        return;
+    }
+    document.getElementById("output").select();
+    document.execCommand("copy");
+    alert("Copied to clipboard");
 }
